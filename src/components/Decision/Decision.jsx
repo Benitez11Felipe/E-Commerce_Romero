5f50bbbed3d7fdbf81e65d01175f6181602ae8cc
Benitez11Felipe <116870493+Benitez11Felipe@users.noreply.github.com>
2024-09-26 19:32:25 -0300

    Add files via upload

---
+++ src/components/Decision/Decision.jsx
@@ -0,0 +1,18 @@
+import "../Decision/Decision.css";
+
+function Decision() {
+  return (
+    <>
+      <div className="seleccionador">
+        <a className="seleccion">
+          <img src="/src/images/buzos/rojo.verde.negro.blanco.gris.png" />
+        </a>
+        <a className="seleccion">
+          <img src="/src/images/pantalones/seleccion2.png" alt="" />
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default Decision;
